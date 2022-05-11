d732044f8a5371a691479a0b79ddf797825c41ea
Łukasz Michalak <lukaszmichalak0402@gmail.com>
2022-05-11 14:54:09 +0200

    changed path

--- src/main/java/com/lukas/hiweb/userdata/UserDataController.java
+++ src/main/java/com/lukas/hiweb/userdata/UserDataController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1")
+@RequestMapping(path = "api/v1/users")
 @RequiredArgsConstructor
 public class UserDataController {
 
